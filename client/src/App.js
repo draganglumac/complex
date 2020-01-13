81381d87e4eab6577117c2f1db851aceda143c12
draganglumac <dragan.glumac@gmail.com>
2020-01-13 17:55:31 +0000

    Saving it all.

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <div>
+            <Link to="/">Home</Link>
+            <span>&nbsp;|&nbsp;</span>
+            <Link to="/otherpage">Other Page</Link>
+          </div>
+          <div>&nbsp;</div>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
